7df2bc37bfe34eb1d3913d2d171d5da13523d113
MIlan Robinson <78048078+MilanrDev@users.noreply.github.com>
2021-02-27 19:34:10 -0500

    Add files via upload

---
+++ MakeUpperCase.js
@@ -0,0 +1,9 @@
+// makeUpperCase
+// Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+  // Code here
+  str = "hello".toUpperCase()
+
+  return  str
+}
