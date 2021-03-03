a5ca5d8f9c0135375ec7c25d87124be2811e74f7
MIlan Robinson <78048078+MilanrDev@users.noreply.github.com>
2021-03-02 23:55:03 -0500

    Add files via upload

---
+++ convertnumtostr.js
@@ -0,0 +1,12 @@
+// Convert a Number to a String!
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+
+function numberToString(num) {
+  let num1 = 123
+
+  return num.toString()
+  // Return a string of the number here!
+}
