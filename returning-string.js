e426e15d48d65731cc7d8b80366b7e2e082ce1e0
MIlan Robinson <78048078+MilanrDev@users.noreply.github.com>
2021-04-07 19:46:54 -0400

    Add files via upload

---
+++ returning-string.js
@@ -0,0 +1,16 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+//
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+
+function greet(name){
+  //your code here
+
+//   p- name
+//   r- "Hello <name> how are you today?"
+//   e- "Hello Vonds how are you today ?"
+//   p- Create a function called greet that takes in a string
+    //   i want function to return a string
+  return (`Hello, ${name} how are you doing today?`)
+
+}
